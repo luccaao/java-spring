3ad3cef6a6b11f369f6fdce3666ea69809b46fe3
lucas <lucasgr01@hotmail.com>
2024-02-25 19:46:48 -0300

    Inicio com java-spring

---
+++ src/main/java/br/com/lucas/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.lucas.screenmatch;
+
+import br.com.lucas.screenmatch.model.DadosSerie;
+import br.com.lucas.screenmatch.service.ConsumoAPI;
+import br.com.lucas.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=1354eb42");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
---
+++ src/main/java/br/com/lucas/screenmatch/service/ConverteDados.java
@@ -0,0 +1,20 @@
+package br.com.lucas.screenmatch.service;
+
+import br.com.lucas.screenmatch.model.DadosSerie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+   private ObjectMapper mapper = new ObjectMapper();
+
+   @Override
+   public <T> T obterDados(String json, Class<T> classe) {
+      try {
+         return mapper.readValue(json, classe);
+      } catch (JsonProcessingException e) {
+         throw new RuntimeException(e);
+      }
+   }
+}
+
+
